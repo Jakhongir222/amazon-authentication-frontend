4a30d38da492eff67214ea237bd334a47599789e
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-02-17 14:43:30 +0100

    Fetching from backend

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,19 +1,43 @@
+import React, { useState } from "react";
+import axios from 'axios';
 
 function LoginForm({onSubmit}) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    try {
+      const response = await axios.post(
+        "http://localhost:8080/api/v1/auth/authenticate",
+        { email, password }
+      );
+
+      localStorage.setItem('token', response.data.token);
+      window.location.reload();
+    } catch (error) {
+      setErrorMessage('Wrong email or password. Please try again.');
+    }
+  }
+
   return (
-      <>
-        <h3>Login</h3>
-        <form onSubmit={onSubmit} className={"login_form"}>
-          <label htmlFor="email_input">Email</label>
-          <input placeholder={"email"} id={"email_input"}/>
-          <br/>
-          <label htmlFor="password_input">Password</label>
-          <input placeholder={"password"} id={"password_input"}/>
-          <br/>
-          <input type={'submit'}/>
-        </form>
-      </>
+    <>
+      <h3>Login</h3>
+      <form onSubmit={handleSubmit} className={"login_form"}>
+        <label htmlFor="email_input">Email</label>
+        <input placeholder={"email"} id={"email_input"} value={email} onChange={(e) => setEmail(e.target.value)} />
+        <br/>
+        <label htmlFor="password_input">Password</label>
+        <input placeholder={"password"} id={"password_input"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+        <br/>
+        <div>{errorMessage}</div>
+        <input type={'submit'}/>
+      </form>
+    </>
   )
 }
 
 export default LoginForm;
+
--- src/components/login/NewUserForm.jsx
+++ src/components/login/NewUserForm.jsx
@@ -1,28 +1,79 @@
+import React, { useState } from 'react';
 
-function NewUserForm({onSubmit}) {
+function NewUserForm() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [confirmedPassword, setConfirmedPassword] = useState('');
+  const [userType, setUserType] = useState('user');
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const user = {
+      email: email,
+      password: password,
+      confirmedPassword: confirmedPassword,
+      userType: userType
+    };
+
+    fetch('http://localhost:8080/api/v1/auth/register', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(user)
+    })
+    .then(response => response.json())
+    .then(data => console.log(data))
+    .catch(error => console.error(error));
+  };
+
   return (
-      <>
-        <h3>Create new user</h3>
-        <form onSubmit={onSubmit} className={"create_user_form"}>
-          <label htmlFor="email_input">Email</label>
-          <input placeholder={"email"} id={"email_input"}/>
-          <br/>
-          <label htmlFor="password_input">Password</label>
-          <input placeholder={"password"} id={"password_input"}/>
-          <br/>
-          <label htmlFor="confirmed_password_input">Confirm password</label>
-          <input placeholder={"confirm password"} id={"confirmed_password_input"}/>
-          <br/>
-          <label htmlFor="type_input">Type of User</label>
-          <select placeholder={"user"} id={"type_input"}>
-            <option value={"user"}>User</option>
-            <option value={"admin"}>Admin</option>
-          </select>
-          <br/>
-          <input type={'submit'}/>
-        </form>
-      </>
-  )
+    <>
+      <h3>Create new user</h3>
+      <form onSubmit={handleSubmit} className="create_user_form">
+        <label htmlFor="email_input">Email</label>
+        <input 
+          type="email"
+          placeholder="email"
+          id="email_input"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+        <br/>
+        <label htmlFor="password_input">Password</label>
+        <input 
+          type="password"
+          placeholder="password"
+          id="password_input"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+        <br/>
+        <label htmlFor="confirmed_password_input">Confirm password</label>
+        <input 
+          type="password"
+          placeholder="confirm password"
+          id="confirmed_password_input"
+          value={confirmedPassword}
+          onChange={(e) => setConfirmedPassword(e.target.value)}
+        />
+        <br/>
+        <label htmlFor="type_input">Type of User</label>
+        <select 
+          placeholder="user"
+          id="type_input"
+          value={userType}
+          onChange={(e) => setUserType(e.target.value)}
+        >
+          <option value="user">User</option>
+          <option value="admin">Admin</option>
+        </select>
+        <br/>
+        <button type="submit">Submit</button>
+      </form>
+    </>
+  );
 }
 
 export default NewUserForm;
