2473cf64c9de7c346f2b6d124aa7c06214be343b
Jakhongir Burkhonov <112249104+Jakhongir222@users.noreply.github.com>
2023-02-18 15:13:11 +0100

    Functionality is working

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -1,43 +1,59 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 import axios from 'axios';
 
-function LoginForm({onSubmit}) {
+function LoginForm() {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
-  const [errorMessage, setErrorMessage] = useState('');
+  const [loggedInUser, setLoggedInUser] = useState(null);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
 
-    try {
-      const response = await axios.post(
-        "http://localhost:8080/api/v1/auth/authenticate",
-        { email, password }
-      );
+    const user = {
+      email: email,
+      password: password
+    };
 
-      localStorage.setItem('token', response.data.token);
-      window.location.reload();
+    try {
+      const response = await axios.post('http://localhost:8080/api/v1/auth/authenticate', user);
+      console.log(response.data);
+      console.log(response.data.email);
+      setLoggedInUser(response.data);
+      console.log(loggedInUser);
     } catch (error) {
-      setErrorMessage('Wrong email or password. Please try again.');
+      console.error(error);
     }
-  }
+  };
 
   return (
     <>
-      <h3>Login</h3>
-      <form onSubmit={handleSubmit} className={"login_form"}>
-        <label htmlFor="email_input">Email</label>
-        <input placeholder={"email"} id={"email_input"} value={email} onChange={(e) => setEmail(e.target.value)} />
-        <br/>
-        <label htmlFor="password_input">Password</label>
-        <input placeholder={"password"} id={"password_input"} type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
-        <br/>
-        <div>{errorMessage}</div>
-        <input type={'submit'}/>
-      </form>
+      {loggedInUser ? (
+        <p>Logged in as {loggedInUser.email}</p>
+      ) : (
+        <form onSubmit={handleSubmit}>
+          <label htmlFor="email_input">Email</label>
+          <input 
+            type="email"
+            placeholder="email"
+            id="email_input"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <br/>
+          <label htmlFor="password_input">Password</label>
+          <input 
+            type="password"
+            placeholder="password"
+            id="password_input"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <br/>
+          <button type="submit">Login</button>
+        </form>
+      )}
     </>
-  )
+  );
 }
 
 export default LoginForm;
-
